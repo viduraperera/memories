7f923445b22cb90c7218cff96564dab632891051
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-04-26 12:47:50 +0530

    creating the submit form 1.0

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {TextField, Button, Typography, Paper} from "@material-ui/core";
+
+import FileBase64 from "react-file-base64";
 
 import useStyles from "./styles";
 
 const From = () =>{
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    });
+
     const classes = useStyles();
+
+    const handleSubmit = () =>{
+
+    }
+
+    const clear = () => {
+
+    }
+
     return(
-        <h1>FORM</h1>
-    )
+        <Paper className={classes.paper}>
+            <form autoComplete={"off"} noValidate className={classes.form} onSubmit={handleSubmit} >
+                <Typography variant={"h6"}>Creating a Memory</Typography>
+
+                <TextField
+                    name = "creator"
+                    variant={"outlined"}
+                    label={"Creator"}
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+
+                <TextField
+                    name = "title"
+                    variant={"outlined"}
+                    label={"Title"}
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+
+                <TextField
+                    name = "message"
+                    variant={"outlined"}
+                    label={"Message"}
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+
+                <TextField
+                    name = "tags"
+                    variant={"outlined"}
+                    label={"Tags"}
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+
+                <div className={classes.fileInput}>
+                    <FileBase64
+                        type={"file"}
+                        multiple={false}
+                        onDone={({base64}) => setPostData({...postData, selectedFile: base64 })}
+                    />
+                </div>
+                <Button  variant={"contained"} color={"secondary"} size={"small"} onClick={clear} fullWidth>Clear</Button>
+                <Button className={classes.buttonSubmit} variant={"contained"} color={"primary"} size={"large"} type={"submit"} fullWidth>Submit</Button>
+
+
+            </form>
+        </Paper>
+    );
 }
 
 export default From;
--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import postRouter from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRouter);
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors);
+
+app.use('/posts', postRouter);
 
 const connection_url = 'mongodb+srv://admin2:admin2123@cluster0.blcwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 8000;
