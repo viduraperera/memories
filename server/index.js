dace594129a11597b1c17aaa9f93258d40759398
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-04-27 04:10:38 +0530

    form is finish have a bug need to fix

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ import postsRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postsRoutes);
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postsRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://vidura:vidura123@cluster0.htogs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
